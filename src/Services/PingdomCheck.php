efc1f4383d3b2d46127a93eaeef1dfdafdaa7165
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2022-07-15 01:59:38 +0700

    minor improvement

--- src/Services/PingdomCheck.php
+++ src/Services/PingdomCheck.php
@@ -14,11 +14,14 @@ class PingdomCheck extends PingdomBase
     /**
      * Get overview of all Pingdom checks
      *
+     * @param integer $offset
+     * @param integer $limit
+     *
      * @return false||array
      */
-    public function getChecks()
+    public function getChecks($offset = 0, $limit = 25000)
     {
-        $path = 'checks?include_tags=true';
+        $path = "checks?include_tags=true&offset=$offset&limit=$limit";
         try {
             $res = $this->client->request('GET', $path);
             $data = json_decode($res->getBody()->getContents(), true);
